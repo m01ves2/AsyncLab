143b953dc9103c558e040a7c835c6c84da40338b
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-05-14 18:08:39 +0300

    Add usual method Work

--- AsyncLab/Program.cs
+++ AsyncLab/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Metrics;
+
+namespace AsyncLab
+{
+    public class Program
+    {
+        public static int counter = 0;
+        public static void Main(string[] args)
+        {
+            Work();
+        }
+
+        public static void Work()
+        {
+            for (int i = 0; i < 1_000_000; i++) {
+                counter++;
+            }
+        }
+    }
+}
