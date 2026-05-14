aa350475bc12ffe3c9b392acdeee726262d00b23
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-05-14 18:10:02 +0300

    Add time execution counter

--- AsyncLab/Program.cs
+++ AsyncLab/Program.cs
@@ -1,13 +1,16 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
 
 namespace AsyncLab
 {
     public class Program
     {
         public static int counter = 0;
+        public static object _lock = new object();
         public static void Main(string[] args)
         {
-            //Work();
+            var sw = Stopwatch.StartNew();
+
             Thread t1 = new Thread(Work);
             Thread t2 = new Thread(Work);
 
@@ -16,12 +19,18 @@ namespace AsyncLab
 
             t1.Join();
             t2.Join();
+
+            sw.Stop();
+            Console.WriteLine(counter);
+            Console.WriteLine($"Time: {sw.ElapsedMilliseconds} ms");
         }
 
         public static void Work()
         {
             for (int i = 0; i < 1_000_000; i++) {
-                counter++;
+                //lock (_lock) {
+                    counter++;
+                //}
             }
         }
     }
