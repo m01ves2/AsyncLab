8cb366c60471a754dad4f3660afac8892a66c476
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-05-17 03:29:27 +0300

    Add async example, thread id change

--- AsyncLab/Program.cs
+++ AsyncLab/Program.cs
@@ -5,32 +5,26 @@ namespace AsyncLab
 {
     public class Program
     {
-        public static int counter = 0;
-        public static object _lock = new object();
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var sw = Stopwatch.StartNew();
-            List<Thread> threads = new List<Thread>();
+            Console.WriteLine($"Main start thread: {Thread.CurrentThread.ManagedThreadId}");
 
-            for (int i = 0; i < 100; i++) {
-                Thread t = new Thread(DoWaitingWork);
-                threads.Add(t);
-                t.Start();
-            }
+            Task task = DoWorkAsync();
 
-            foreach (Thread t in threads) {
-                t.Join();
-            }
+            Console.WriteLine("Task started");
 
+            await task;
 
-            sw.Stop();
-            Console.WriteLine(counter);
-            Console.WriteLine($"Time: {sw.ElapsedMilliseconds} ms"); //3000 msec
+            Console.WriteLine($"Main resumed thread: {Thread.CurrentThread.ManagedThreadId}");
         }
 
-        public static void DoWaitingWork()
+        public static async Task DoWorkAsync()
         {
-            Thread.Sleep(3000);
+            Console.WriteLine($"Before await: {Thread.CurrentThread.ManagedThreadId}");
+
+            await Task.Delay(2000);
+
+            Console.WriteLine($"After await: {Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
