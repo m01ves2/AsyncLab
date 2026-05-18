bf4d8fbaecb7210b4ad29ddedf1dad566c779aab
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-05-18 20:14:19 +0300

    Add .NET Web API project. Add TestController with not blocking Get-method

---
+++ WebAPI/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            Console.WriteLine(
+                $"START Thread: {Thread.CurrentThread.ManagedThreadId}");
+
+            await Task.Delay(5000);
+
+            Console.WriteLine(
+                $"END Thread: {Thread.CurrentThread.ManagedThreadId}");
+
+            return Ok();
+        }
+    }
+}
