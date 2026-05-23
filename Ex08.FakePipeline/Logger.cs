76366b3da116ffd75f5a179e21b0add88be75602
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-05-24 02:29:46 +0300

    Add exercise-8. Investigate how ASP.NET Core middleware pipeline works

---
+++ Ex08.FakePipeline/Logger.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex08.FakePipeline
+{
+    internal class Logger
+    {
+        public static void Log(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Ex08.FakePipeline/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Ex08.FakePipeline
+{
+    public delegate Task RequestDelegate();
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await LoggingMiddleware(
+                    () => AuthMiddleware(
+                        () => EndpointHandler()
+                        )
+                  );
+        }
+
+        private static async Task LoggingMiddleware(RequestDelegate next)
+        {
+            Logger.Log("Before logging");
+            Logger.Log($"[Thread {Thread.CurrentThread.ManagedThreadId}]");
+            await Task.Delay(1000);
+            await next();
+            Logger.Log($"[Thread {Thread.CurrentThread.ManagedThreadId}]");
+            Logger.Log("After logging");
+        }
+
+        private static async Task AuthMiddleware(RequestDelegate next)
+        {
+            Logger.Log("Before auth");
+            Logger.Log($"[Thread {Thread.CurrentThread.ManagedThreadId}]");
+            await Task.Delay(1000);
+            await next();
+            Logger.Log($"[Thread {Thread.CurrentThread.ManagedThreadId}]");
+            Logger.Log("After auth");
+        }
+
+        private static async Task EndpointHandler()
+        {
+            Logger.Log("Before endpoint");
+            Logger.Log($"[Thread {Thread.CurrentThread.ManagedThreadId}]");
+            Logger.Log("Endpoint processing");
+            await Task.Delay(1000);
+            Logger.Log($"[Thread {Thread.CurrentThread.ManagedThreadId}]");
+            Logger.Log("After endpoint");
+        }
+
+    }
+}
